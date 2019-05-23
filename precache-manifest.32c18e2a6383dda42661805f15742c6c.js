b17d6b1e01437e8dd72633bb92397effba7ae992
Crazyglue <edbz2k2@gmail.com>
2019-05-22 22:16:46 -0500

    Updates

---
+++ precache-manifest.32c18e2a6383dda42661805f15742c6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f5f5d44b537f5d99c500e1b5d85ca3",
+    "url": "/seed/index.html"
+  },
+  {
+    "revision": "dce5ce47fa4724976141",
+    "url": "/seed/static/css/main.5ba6497c.chunk.css"
+  },
+  {
+    "revision": "1682daa00100bbef2980",
+    "url": "/seed/static/js/2.68654217.chunk.js"
+  },
+  {
+    "revision": "dce5ce47fa4724976141",
+    "url": "/seed/static/js/main.381ad672.chunk.js"
+  },
+  {
+    "revision": "04727000fa23783ba454",
+    "url": "/seed/static/js/runtime~main.a0f35e1e.js"
+  }
+]);
